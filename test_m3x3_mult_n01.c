382644cba61bd431b3faa86b58aa4649575f0b7e
H. Gabriel Máculus <gabrielmaculus@gmail.com>
2026-04-06 08:35:42 -0300

    test_m3x3_mult_n01.c: working

--- test_m3x3_mult_n01.c
+++ test_m3x3_mult_n01.c
@@ -3,23 +3,19 @@
 int main(int argc, char const *argv[])
 {
 
-    puts("test_suma:");
+    puts("test_mult_n: Multiplica una matriz por un numero N");
     m3x3 a = {
         1,2,3,
         4,5,6,
         7,8,9
     };
-    m3x3 b = {
-        1,2,3,
-        4,5,6,
-        7,8,9
-    };
+
     m3x3 r;
     m3x3_imprimir(a);
-    puts("+");
-    m3x3_imprimir(b);
+    puts("*");
+    puts("7");
     
-    m3x3_suma(a, b, r);
+    m3x3_mult_n(a, 7, r);
     puts("Resultado:");
 
     m3x3_imprimir(r);
