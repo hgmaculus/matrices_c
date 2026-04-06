1e4e6deef6e44b7282a41fba6db9649f7b264f21
H. Gabriel Máculus <gabrielmaculus@gmail.com>
2026-04-06 08:32:19 -0300

    test_m3x3_mult_n01.c: add

---
+++ test_m3x3_mult_n01.c
@@ -0,0 +1,27 @@
+#include "matrices.h"
+
+int main(int argc, char const *argv[])
+{
+
+    puts("test_suma:");
+    m3x3 a = {
+        1,2,3,
+        4,5,6,
+        7,8,9
+    };
+    m3x3 b = {
+        1,2,3,
+        4,5,6,
+        7,8,9
+    };
+    m3x3 r;
+    m3x3_imprimir(a);
+    puts("+");
+    m3x3_imprimir(b);
+    
+    m3x3_suma(a, b, r);
+    puts("Resultado:");
+
+    m3x3_imprimir(r);
+    return 0;
+}
